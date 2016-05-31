96b76329307f3f72ed5cef5c36af6db244cbb303
Sille Van Landschoot <info@sillevl.be>
2016-05-31 21:14:49 +0200

    add first tests

--- test/robotwars_test.rb
+++ test/robotwars_test.rb
@@ -1,11 +1,31 @@
 require 'test_helper'
 
-class RobotwarsTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Robotwars::VERSION
+
+describe Game do
+  before { Game.setup }
+
+  describe "#setup" do
+    it "has a game window" do
+      Game.game_window.must_be_instance_of GameWindow
+    end
+
+    it "has an input manager" do
+      expect(Game.input_manager).must_be_instance_of InputManager
+    end
+
+    it "has an input manager with a target" do
+      expect(Game.input_manager.target).must_be_instance_of GameWindow
+    end
+
+    it "has a scene" do
+      expect(Game.scene).must_be_instance_of Scene
+    end
   end
 
-  def test_it_does_something_useful
-    assert false
+  describe "#start" do
+    it "shows the game window" do
+      #expect(GameWindow).must_send(:show)
+      Game.start
+    end
   end
 end

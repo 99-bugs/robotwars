a6637cc13cade0739db4d9feb90674e4bc3ab484
Sille Van Landschoot <info@sillevl.be>
2016-06-02 19:51:14 +0200

    move sandbags for better placement on map

--- lib/robotwars/scene.rb
+++ lib/robotwars/scene.rb
@@ -5,7 +5,7 @@ module Robotwars
 		include Robotwars
 		def initialize
 			Robotwars::log.debug "create new GameScreen"
-			@robots = Array.new
+			@robots = create_robots
 			@battleground = Battleground.new
 			@ranking = Ranking.new @robots
 		end
@@ -19,6 +19,9 @@ module Robotwars
 			Gosu::translate 16,0 do
 				Gosu::clip_to(0,0,1536 + 32,768 + 32) do
 					@battleground.draw
+					@robots.each do |robot|
+						robot.draw
+					end
 				end
 			end
 			Gosu::translate 0,800 do
@@ -26,6 +29,20 @@ module Robotwars
 					@ranking.draw
 				end
 			end
+		end
+
+		def create_robots
+			robots = Array.new
+			robot = Robot.new
+			robot.update_position Geometry::Point[100,100], 45
+			robots << robot
+			robot = Robot.new
+			robot.update_position Geometry::Point[300,100], 45
+			robots << robot
+			robot = Robot.new
+			robot.update_position Geometry::Point[500,500], 0
+			robots << robot
+			robots
 		end
 	end
 end

58753c1b2c66e6cd89683fe1efa95ba6fb63c738
Sille Van Landschoot <info@sillevl.be>
2016-06-02 16:28:07 +0200

    add barrel to robot tank

--- lib/robotwars/robot.rb
+++ lib/robotwars/robot.rb
@@ -19,12 +19,14 @@ class Robot
         reset
         update_position Point[0,0], 0
         @lastshot = DateTime.now - 10
-				@image = Gosu::Image.new "assets/images/Tanks/tankBlue.png"
+		@image = Gosu::Image.new "assets/images/Tanks/tankBlue.png"
+        @barrel = Gosu::Image.new "assets/images/Tanks/barrelBlue.png"
     end
 
-		def draw
-			@image.draw_rot @position.x, @position.y, 1, @heading
-		end
+	def draw
+		@image.draw_rot @position.x, @position.y, 1, @heading, 0.5, 0.5, 0.7, 0.7
+        @barrel.draw_rot @position.x, @position.y, 10, @heading, 0.5, 0.5, 0.7, 0.7
+	end
 
     def shoot type = "rocket"
         if @lastshot + 5.0 < DateTime.now
